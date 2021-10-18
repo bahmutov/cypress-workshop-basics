59a3b985dc74b03616b65969d0295519ae59edff
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-10-18 14:50:15 -0400

    small changes

--- cypress/integration/05-xhr/answer.js
+++ cypress/integration/05-xhr/answer.js
@@ -258,21 +258,30 @@ describe('waits for network idle', () => {
     // load the page, but delay loading of the data by some random number
     // using /?delay=<number> query param
     const delayMs = Cypress._.random(100, 1500)
-    cy.visit(`/?delay=${delayMs}`)
+    cy.visit(`/?delay=${delayMs}`).then(() => {
+      // start waiting after the cy.visit command finishes
 
-    // wait for network to be idle for 1 second
-    // using a .should(cb) assertion that looks at the current timestamp
-    // vs the timestamp of the last network call
-    // see assertion examples at
-    // https://glebbahmutov.com/cypress-examples/commands/assertions.html
-    // TIP: cy.wrap('message').should(cb) works really well
-    const started = +new Date()
-    cy.wrap('network idle for 2 sec').should(() => {
-      const t = lastNetworkAt || started
-      const elapsed = +new Date() - t
-      if (elapsed < 2000) {
-        throw new Error('Network is busy')
-      }
+      // wait for network to be idle for 1 second
+      // using a .should(cb) assertion that looks at the current timestamp
+      // vs the timestamp of the last network call
+      // see assertion examples at
+      // https://glebbahmutov.com/cypress-examples/commands/assertions.html
+      // TIP: cy.wrap('message').should(cb) works really well
+      const started = +new Date()
+      let finished
+      cy.wrap('network idle for 2 sec')
+        .should(() => {
+          const t = lastNetworkAt || started
+          const elapsed = +new Date() - t
+          if (elapsed < 2000) {
+            throw new Error('Network is busy')
+          }
+          finished = +new Date()
+        })
+        .then(() => {
+          const waited = finished - started
+          cy.log(`finished after ${waited} ms`)
+        })
     })
     // by now everything should have been loaded
     // we can check the page and use a very short timeout
