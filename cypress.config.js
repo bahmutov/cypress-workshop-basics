41032eb6bf1985a65a1a254dad5b10aaea2c4986
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-02-16 13:46:11 -0500

    upgrade Cypress v12

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  viewportWidth: 600,
+  viewportHeight: 800,
+  experimentalStudio: true,
+  experimentalInteractiveRunEvents: true,
+  projectId: '89mmxs',
+  e2e: {
+    experimentalRunAllSpecs: true,
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    specPattern: ['cypress/e2e/*/spec.js', 'cypress/e2e/*/demo.js'],
+    baseUrl: 'http://localhost:3000'
+  }
+})
