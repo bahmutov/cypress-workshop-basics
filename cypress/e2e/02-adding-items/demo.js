552e8eb094bc3fe1e34ef2957e06dc87ab919f6e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-03-27 13:27:02 -0400

    rest easy demo

--- cypress/e2e/02-adding-items/demo.js
+++ cypress/e2e/02-adding-items/demo.js
@@ -1,30 +1,30 @@
 /// <reference types="cypress" />
-const isLocalHost = () => Cypress.config('baseUrl').includes('localhost')
 
-if (isLocalHost()) {
-  // we can reset data only when running locally
-  beforeEach(function resetData() {
-    cy.request('POST', '/reset', {
-      todos: []
+import 'cypress-rest-easy'
+
+describe('TodoMVC', { baseUrl: null, rest: { todos: [] } }, () => {
+  it.only('adds items', function () {
+    const todos = Cypress.env('todos')
+    cy.visit('todomvc/index.html')
+    cy.get('.new-todo')
+      .type('todo A{enter}')
+      .type('todo B{enter}')
+      .type('todo C{enter}')
+      .type('todo D{enter}')
+    cy.get('.todo-list li') // command
+      .should('have.length', 4) // assertion
+    cy.log('**complete items**')
+    cy.contains('[data-cy="remaining-count"]', '4')
+    cy.contains('.todo', 'todo B').find('.toggle').click()
+    cy.contains('[data-cy="remaining-count"]', '3').then(() => {
+      expect(todos, '4 items').to.have.length(4)
+      console.table(todos)
     })
   })
-}
 
-beforeEach(function visitSite() {
-  cy.log('Visiting', Cypress.config('baseUrl'))
-  cy.visit('/')
-})
-
-it('adds items', function () {
-  cy.get('.new-todo')
-    .type('todo A{enter}')
-    .type('todo B{enter}')
-    .type('todo C{enter}')
-    .type('todo D{enter}')
-  cy.get('.todo-list li') // command
-    .should('have.length', 4) // assertion
-  cy.log('**complete items**')
-  cy.contains('[data-cy="remaining-count"]', '4')
-  cy.contains('.todo', 'todo B').find('.toggle').click()
-  cy.contains('[data-cy="remaining-count"]', '3')
+  it.only('starts with zero items', () => {
+    cy.visit('todomvc/index.html')
+    cy.get('.loaded')
+    cy.get('li.todo').should('have.length', 0)
+  })
 })
